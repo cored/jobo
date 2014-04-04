1f72190be196815532c4d09d1f5508a9f40012e3
Rafael George <george.rafael@gmail.com>
2014-04-04 18:37:40 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Jobo::Application.config.secret_key_base = '602f877ce07d3a4774ddd91676dc7e26bf6fac566fb0b11219386ba78dabeebec492fec7b91fa5530c650390aa17746b8320a65e7ec4392af96b738b4eb43f91'
